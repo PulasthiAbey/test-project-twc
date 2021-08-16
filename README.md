99e3c7b2e19091ae663146c208e1d6743d9de8ad
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-08-16 19:40:18 +0530

    changes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Test Project TWC Innovations
 *This is the interview repository for the TWC Innovation*
+
+## 1. Install following packages before you get started
+1. `npm i react-router-dom`
---
+++ dummy-app/src/components/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+function home() {
+  return (
+    <div className="row">
+      <div className="col col-left"></div>
+      <div className="col col-left"></div>
+    </div>
+  );
+}
+
+export default home;
